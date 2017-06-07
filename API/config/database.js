0ff02e8be30a1c4fac875bc69defa1112f0e0b72
Mike D <mdarts@gmail.com>
2017-06-06 22:14:48 -0600

    Add files via upload

---
+++ API/config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb://homer:simpson@ds155961.mlab.com:55961/duff' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
