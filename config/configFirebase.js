0a68d04d4aadac91c1369894e84fff9ff6d1164a
mrchadparkour <cplattkuhn@gmail.com>
2017-07-05 16:03:17 -0600

    updated read me

--- config/configFirebase.js
+++ config/configFirebase.js
@@ -1,12 +1,7 @@
 import firebase from 'firebase';
 
 let config = {
-  apiKey: "AIzaSyAKVhOleqXW-yl9bCzCcEvhno3z0quGCwE",
-  authDomain: "brewtour-66745.firebaseapp.com",
-  databaseURL: "https://brewtour-66745.firebaseio.com",
-  projectId: "brewtour-66745",
-  storageBucket: "brewtour-66745.appspot.com",
-  messagingSenderId: "969813913995"
+ //paste firebase config here
 };
 const firebaseApp = firebase.initializeApp(config);
 
